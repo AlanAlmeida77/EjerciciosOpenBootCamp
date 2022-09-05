0c15c171b2dad986488f5dc5a8cf1bc52b1aa9f7
Alan Almeida <alandavidalmeida@gmail.com>
2022-09-05 02:12:38 -0300

    https://github.com/AlanAlmeida77/EjerciciosOpenBootCamp.git

---
+++ EjerciciosOpenBootCamp/untitled/src/Suma.java
@@ -0,0 +1,9 @@
+class Suma {
+
+    public static void main( String[] args ) {
+        int a = 20, b = 30, c = 50, d = 100;
+        d = a + b + c;
+        System.out.println( "La suma de a, b y c es: "+ d);
+    }
+
+}
